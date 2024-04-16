54f432da0fddf08884a0bd5a2958d87a2f8c7536
Salihou18 <skingue0906@gmail.com>
2024-04-15 21:22:06 -0400

    validation formulaire contactez-nous

---
+++ scriptPart2/scriptContact.js
@@ -0,0 +1,91 @@
+// Utiliser de chargement du DOM
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // //les regex
+    const prenomRegex = /^[a-zA-Z]{2,}$/
+    const nomRegex = /^[a-zA-Z]{2,}$/
+    const regexEmail = /\S+@\S+\.\S+/;
+    const rueRegex = /^[\d]+ ([a-zA-Z]{1,})+$/ 
+    const villeRegex = /^[a-zA-Z]{2,}$/
+
+    //Cibler le formulaire dans le document
+
+    const formulaire = document.querySelector("#seulform");
+    console.log(formulaire)
+
+    //recuperation des champs a travers leurs noms en une seule ligne dans la variable formulaire
+
+    // const { prenom, nom, email, rue, ville } = formulaire.elements;
+
+    // Création  d'un objet client avec les valeurs récupérées dans les champs du formulaire
+
+    const client = {
+        prenom: prenom.value,
+        nom: nom.value,
+        email: email.value,
+        rue: rue.value,
+        ville: ville.value
+    };
+
+
+    console.log(client);
+
+    //validation des champs
+
+    const champsvalide = (champ) => {
+
+        switch (champ) {
+            case 'prenom': {
+                if (!prenomRegex.test(prenom.value)) {
+                    alert(`Le champ ${champ} est invalide`);
+                }
+                break;
+            }
+            case 'nom': {
+                if (!nomRegex.test(nom.value)) {
+                    alert(`Le champ ${champ} est invalide`);
+                }
+                break;
+            }
+            case 'email': {
+                if (!regexEmail.test(email.value)) {
+                    alert(`Le champ ${champ} est invalide`);
+                }
+                break;
+            }
+            case 'rue': {
+                if (!rueRegex.test(rue.value)) {
+                    alert(`Le champ ${champ} est invalide`);
+                }
+                break;
+            }
+            case 'ville': {
+                if (!villeRegex.test(ville.value)) {
+                    alert(`Le champ ${champ} est invalide`);
+                }
+                break;
+            }
+            default:
+        }
+        
+           
+        
+    }
+    formulaire.addEventListener("submit", (e) => {
+        //Changer le comportement par defaut d'un formulaire
+        e.preventDefault()
+
+        //verification des infos rentrees dans le formulaire
+        for (let champ in client) {
+            champsvalide(champ)
+        }
+         // Si toutes les validations passent, le formulaire est soumis
+         alert("Formulaire soumis avec succès !");
+
+    })
+
+
+
+
+})
